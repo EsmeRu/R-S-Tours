e45d221264ca3953dece5b9c5ac04000b6daba40
EsmeRu <rubin.esmef@gmail.com>
2020-10-10 21:54:05 -0600

    header, navegacion y banner

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/* HEADER - BARRA DE NAVEGACIÓN */
+window.onscroll = function() {navFunction()};
+
+const header = document.getElementById("navHeader");
+const sticky = header.offsetTop;
+
+function navFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+/* BANNER */
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 10000); // Change image every 2 seconds
+}
