9c342b32bba026b889ace6dbfe2399e7dbffc81e
EsmeRu <rubin.esmef@gmail.com>
2020-10-10 23:43:47 -0600

    imagenes banner, info

--- js/main.js
+++ js/main.js
@@ -24,5 +24,5 @@ function carousel() {
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}    
   x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 10000); // Change image every 2 seconds
+  setTimeout(carousel, 7000); // Change image every 2 seconds
 }
