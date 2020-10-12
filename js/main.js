cc71f50fc5f5a58eb40348ad3bcd3a6b690f1ecf
EsmeRu <rubin.esmef@gmail.com>
2020-10-12 01:06:58 -0600

    menu

--- js/main.js
+++ js/main.js
@@ -12,6 +12,14 @@ function navFunction() {
     header.classList.remove("sticky");
   }
 }
+// MENÚ HAMBURGUESA
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
 
 /* BANNER */
 var myIndex = 0;
