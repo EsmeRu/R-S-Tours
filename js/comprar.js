0d76f4f920fac843b872eb0cf1e5413fc8e10766
Christian Salas <ch230700@gmail.com>
2020-11-16 15:40:11 -0700

    compras 0.1%

---
+++ js/comprar.js
@@ -0,0 +1,8 @@
+const carrito = []
+const carritoLocal = JSON.parse(localStorage.getItem('buyCar'))
+const tourList = document.getElementById('tour-list')
+
+carritoLocal.forEach(tour => {
+    const
+    
+});
