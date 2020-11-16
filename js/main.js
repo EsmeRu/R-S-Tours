8512d71ec1dfceff40351ef7d5eb68cf1c2dde6b
Christian Salas <ch230700@gmail.com>
2020-11-16 04:31:46 -0700

    cargar correo y nombre a contacto

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-/** AÑADIR AL CARRITO */
+/** AÑADIR AL CARRITO 
 $('#addCart').click( () => {
 
-  
-
   alert("Se añadió al carrito")
-});
+});*/
 
 
 /* HEADER - BARRA DE NAVEGACIÓN */
@@ -38,11 +36,11 @@ function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    /**x[i].style.display = "none";  */
   }
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
+  /**x[myIndex-1].style.display = "block";  */
   setTimeout(carousel, 5000); // Change image every 2 seconds
 }
 
